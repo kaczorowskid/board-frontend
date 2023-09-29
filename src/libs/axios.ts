b23787386253904dd6b3c1e37c62793228753796
Damian Kaczorowski <57078565+kaczorowskid@users.noreply.github.com>
2023-09-29 15:32:37 +0200

    feat: add vite-plugin-environment (#24)

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,10 @@
 import { userToken } from 'constants/token';
 import axios from 'axios';
 
-const baseURL = 'http://localhost:4200';
+const baseURL =
+  process.env.VITE_ENVIRONMENT === 'development'
+    ? process.env.VITE_API_DEVELOPMENT_URL
+    : process.env.VITE_API_PROD_URL;
 
 const headers = {
   'Content-Type': 'application/json',
