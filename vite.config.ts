da09da8cb025ea90532f3caca8273e3939c188a0
sijab <kaczorowskid97@gmail.com>
2023-09-29 15:31:33 +0200

    feat: add vite-plugin-environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 export default defineConfig(({ command }) => ({
   plugins: [
     react(),
     viteTsconfigPaths(),
+    EnvironmentPlugin('all'),
     command !== 'build'
-      ? checker({ typescript: true, eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' } })
+      ? checker({
+          typescript: true,
+          eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' }
+        })
       : undefined
   ],
   server: {
