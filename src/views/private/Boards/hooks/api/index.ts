5440514620ac8d662d48b8e34063b5587e0cee39
Damian Kaczorowski <57078565+kaczorowskid@users.noreply.github.com>
2023-10-15 12:57:07 +0200

    refactor: hooks refactor (#35)

---
+++ src/views/private/Boards/hooks/api/index.ts
@@ -0,0 +1,6 @@
+export * from './useConfirrmShareToken.hook';
+export * from './useCreateBoard.hook';
+export * from './useCreateShareToken.hook';
+export * from './useEditBoard.hook';
+export * from './useFetchBoards.hook';
+export * from './useRemoveBoard.hook';
